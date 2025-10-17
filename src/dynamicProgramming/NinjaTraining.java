b6d7d322a80c58d77b6e9f39a60da0825b07c180
SatyamHans <satyamhans@gmail.com>
2025-10-17 14:58:35 +0530

    ninja training tabulation approach

--- src/dynamicProgramming/NinjaTraining.java
+++ src/dynamicProgramming/NinjaTraining.java
@@ -58,12 +58,34 @@ package dynamicProgramming;
 import java.util.Arrays;
 
 public class NinjaTraining {
+    //memoisation approach
     public static int ninjaTraining(int n,int[][] points){
         int [][] dp=new int[n][4];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+//        for(int[] row:dp){
+//            Arrays.fill(row,-1);
+//        }
+//        return solve(n-1,3,points,dp);
+
+        //tabulation approach
+        dp[0][0]=Math.max(points[0][1],points[0][2]);
+        dp[0][1]=Math.max(points[0][0],points[0][2]);
+        dp[0][2]=Math.max(points[0][0],points[0][1]);
+        dp[0][3]=Math.max(points[0][0],Math.max(points[0][1],points[0][2]));
+
+        for(int day=1;day<n;day++){
+            for(int last=0;last<4;last++){
+                dp[day][last]=0;
+                for(int task=0;task<2;task++){
+                    if(task!=last){
+                        int curr=points[day][task]+dp[day-1][task];
+                        dp[day][last]=Math.max(dp[day][last],curr);
+                    }
+                }
+         }
         }
-        return solve(n-1,3,points,dp);
+        return dp[n-1][3];
+
+
 
     }
     public static int solve(int day,int last,int [][] points,int dp[][]){
